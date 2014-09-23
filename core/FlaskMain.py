7b9226790708ef71d2569608d62e3e8ede56ee27
jerryj <javalove93@gmail.com>
2014-09-23 21:18:11 +0900

    use FlaskMain.py's location as root dir

--- core/FlaskMain.py
+++ core/FlaskMain.py
@@ -8,12 +8,14 @@ import json
 import codecs
 import uuid
 import requests
+import os
 
 app = Flask(__name__)
+mainDir = os.path.dirname(os.path.abspath(__file__))
 
 def read_repository(name):
 	print "Name: " + name
-	f = codecs.open(name + ".json", 'r', encoding='utf8')
+	f = codecs.open(mainDir + '/' + name + ".json", 'r', encoding='utf8')
 	data = f.read()
 	f.close()
 # 	print data
@@ -22,7 +24,7 @@ def read_repository(name):
 
 def write_repository(name, data):
 	print "Name: " + name
-	f = codecs.open(name + ".new.json", 'w', encoding='utf8')
+	f = codecs.open(mainDir + '/' + name + ".new.json", 'w', encoding='utf8')
 	data = f.write(json.dumps(data))
 	f.close()
 
