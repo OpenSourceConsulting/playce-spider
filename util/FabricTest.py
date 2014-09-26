c8979f74b6f83e2b644fcc5481b85b1a3a455153
jerryj <javalove93@gmail.com>
2014-09-26 13:57:33 +0900

    case 207

--- util/FabricTest.py
+++ util/FabricTest.py
@@ -4,7 +4,7 @@ Created on 2014. 9. 11.
 @author: jerryj
 '''
 
-from fabric.api import run, sudo, env
+from fabric.api import run, sudo, env, put, cd
 from fabric.tasks import execute
 import rpyc
 import json
@@ -36,21 +36,21 @@ def show_interfaces():
 				print parsed
 
 def show_interfaces_with_configure():
-	result = run('configure', pty=False)
-	result = run('show interfaces', pty=False)
+	f = open('./commands.txt', 'w')
+	commands = [
+			'$SET interfaces loopback lo address 127.0.0.5/24',
+			'$COMMIT',
+			'$SHOW interfaces'
+			]
+	f.write("; ".join(commands))
+	f.close()
+	with cd('/home/vyatta/test/scripts'):
+		put('./cli.txt', 'cli.sh', mode=0755)
+		put('./commands.txt', 'commands.sh', mode=0755)
+		result = run('./cli.sh', pty=False)
 	lines = result.split('\n')
-	conn = rpyc.connect("centos7", 18881)
-	conn.root.add_pattern('VYATTA_ETH_STATUS', '[uDA]')
-	foundHeader = False
 	for line in lines:
 		print "LINE: " + line
-		if conn.root.parse(line, r"Interface%{SPACE}IP Address%{SPACE}S/L  Description"):
-			foundHeader = True
-		
-		if foundHeader:
-			parsed = conn.root.parse(line, r"%{USERNAME:nic_name}%{SPACE:}%{IP:addr}/%{NUMBER:subnet}%{SPACE:}%{VYATTA_ETH_STATUS:state}/%{VYATTA_ETH_STATUS:link}")
-			if parsed != None:
-				print parsed
 
 def locale():
 	run('locale')
@@ -58,4 +58,4 @@ def locale():
 if __name__ == "__main__":
 	execute(dev)
 # 	execute(locale)
-	execute(show_interfaces, hosts=['vyatta'])
+	execute(show_interfaces_with_configure, hosts=['vyatta'])
