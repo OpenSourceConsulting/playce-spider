3c95999f7ff99986c128725e1f8d74165d769402
jerryj <javalove93@gmail.com>
2014-10-01 22:46:14 +0900

    Case: 271 Provisioning Script

---
+++ util/init.sh
@@ -0,0 +1,24 @@
+# /etc/spider/init.sh
+# Vyatta VM Initial Provisioning Script
+
+IFCFG=`ifconfig -a`
+ROUTE=`netstat -nr`
+VBASH=`vbash --version`
+UNAME=`uname -a`
+
+cat > /tmp/init.sh.tmp << EOF
+### IFCONFIG
+$IFCFG
+### ROUTE
+$ROUTE
+### VBASH
+$VBASH
+### UNAME
+$UNAME
+### END
+EOF
+
+curl -X POST -H "Content-Type: text/plain" http://spider-controller:5001/vmreg --data-binary @/tmp/init.sh.tmp
+
+rm /tmp/init.sh.tmp
+
