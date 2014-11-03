a7b66b36d9e251a9e8cc796fc26377977d9e974d
jerryj <javalove93@gmail.com>
2014-11-04 03:00:15 +0900

    Fabric 코드를 __init__.py로 이동

--- core/spidercore/__init__.py
+++ core/spidercore/__init__.py
@@ -4,11 +4,45 @@ import codecs
 import json
 from flask_cors import CORS
 import logging.config
+from pyparsing import *
 
+#	Flask initialization
 app = Flask(__name__)
 cors = CORS(app, headers='Content-Type', methods=['HEAD', 'OPTIONS', 'POST', 'GET', 'DELETE', 'PUT'])
 
+#	Main directory
 mainDir = os.path.dirname(os.path.abspath(__file__))
+
+
+keywords = CaselessKeyword('interfaces') | CaselessKeyword('nat') | CaselessKeyword('service') | CaselessKeyword('system')
+elementList = Forward()
+string = Word( alphanums+'_-:/.+@$' )
+number = Combine( Optional('-') + ( '0' | Word('123456789',nums) ) +
+                   Optional( '.' + Word(nums) ) +
+                   Optional( Word('eE',exact=1) + Word(nums+'+-',nums) ) )
+value = string | number | dblQuotedString.setParseAction(removeQuotes)
+unaryItem = Group(value + Suppress(lineEnd()))
+item = Group(string + value + Suppress(lineEnd()))
+element = Forward()
+itemOrElement = item | unaryItem | element
+element << Group(string + Optional(string) + Group(Suppress('{') + ZeroOrMore(itemOrElement) + Suppress('}')))
+elementList = ZeroOrMore(itemOrElement)
+rootKeywords = keywords + Group( Suppress('{') + elementList + Suppress('}'))
+config = OneOrMore(rootKeywords)
+vbash_message = Regex(r"vbash\:.*").setName("vbash message")
+config.ignore(vbash_message)
+
+
+def convertNumbers(s,l,toks):
+	n = toks[0]
+	try:
+		return int(n)
+	except ValueError, ve:
+		return float(n)
+	
+number.setParseAction( convertNumbers )
+
+
 
 def setup_logging(
     default_path='conf/log_conf.json', 
