7da1d0390628201b809ff778569b9e8dfc712bf2
jerryj <javalove93@gmail.com>
2014-11-04 02:44:47 +0900

    Case 414: Python logger 적용

--- core/spidercore/__init__.py
+++ core/spidercore/__init__.py
@@ -3,11 +3,33 @@ import os
 import codecs
 import json
 from flask_cors import CORS
+import logging.config
 
 app = Flask(__name__)
 cors = CORS(app, headers='Content-Type', methods=['HEAD', 'OPTIONS', 'POST', 'GET', 'DELETE', 'PUT'])
 
 mainDir = os.path.dirname(os.path.abspath(__file__))
+
+def setup_logging(
+    default_path='conf/log_conf.json', 
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """ 
+    Setup logging configuration
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+        logging.info("logging: load "+ os.path.abspath(path))
+    else:
+        logging.basicConfig(level=default_level)
+        logging.info("logging: set basic config.")
 
 def read_repository(name):
 	print "Name: " + name
