08a69da8fe126660575949b921bec88d49ce45c1
nices96 <nices96@gmail.com>
2014-11-07 15:03:23 +0900

    사용자 관련 API 소스 위치 변경

--- core/spidercore/FlaskMain.py
+++ core/spidercore/FlaskMain.py
@@ -128,6 +128,61 @@ def mon_vmhost(id=None):
 			
 	return json.dumps(results)
 
+# User Login
+@app.route("/user/login", methods=['POST'])
+def login():
+	jsonData = request.json
+	
+	userId = jsonData.get('userId')
+	password = jsonData.get('password')
+	
+	logger.debug("userId : [%s], Password : [%s]", userId, password)
+	
+	users = read_repository("users")
+	
+	match = False
+	for user in users:
+		if user['userId'] == userId and user['password'] == password:
+			match = True
+			break
+	
+	if match:
+		return json.dumps(user)
+	else:
+		return 'Invalid userId or password.', 404
+
+# Get User List
+@app.route("/user/list", methods=['GET'])
+def user_list():
+	users = read_repository("users")
+	return json.dumps(users)
+
+# Add User
+@app.route("/user/insert", methods=['POST'])
+def user_insert():
+	jsonData = request.json
+	
+	if jsonData.get('userId') == None:
+		return "userId can not be null.", 503
+	elif jsonData.get('password') == None:
+		return "password can not be null.", 503
+	else:
+		users = read_repository("users")
+	
+	# duplication check
+	match = False
+	for user in users:
+		if user['userId'] == jsonData.get('userId') and user['password'] == jsonData.get('password'):
+			match = True
+			break
+	
+	if match:
+		return jsonData.get('userId') + " is already exists."
+	else:
+		users.append(jsonData)
+		write_repository("users", users)
+		return json.dumps(users)
+
 
 if __name__ == "__main__":
 	logger.critical('Server Starting...')
