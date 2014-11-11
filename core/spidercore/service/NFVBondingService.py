7ed35703269a3f931225d04bc18763c8a5bd26fb
nices96 <nices96@gmail.com>
2014-11-11 09:10:09 +0900

    .

--- core/spidercore/service/NFVBondingService.py
+++ core/spidercore/service/NFVBondingService.py
@@ -5,7 +5,7 @@
 #                                                                                              #
 # This file is part of athena-spider. https://github.com/OpenSourceConsulting/athena-spider    #
 #                                                                                              #
-# PyGithub is free software: you can redistribute it and/or modify it under                    #
+# athena-spider is free software: you can redistribute it and/or modify it under               #
 # the terms of the GNU Lesser General Public License as published by the Free                  #
 # Software Foundation, either version 3 of the License, or (at your option)                    #
 # any later version.                                                                           #
