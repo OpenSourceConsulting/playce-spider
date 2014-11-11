afe9007f2ca00624bda622e603f2edd673dfc231
jerryj <javalove93@gmail.com>
2014-11-11 18:05:13 +0900

    ....

--- core/spidercore/FlaskMainNFV.py
+++ core/spidercore/FlaskMainNFV.py
@@ -153,7 +153,7 @@ def vm_reg_init():
 				if 'interim' in vm and vm['interim']:
 					vms[i] = jsonData
 					break
-				elif 'mgraddr' in vms and vm['mgraddr'] != ipAddr:
+				elif 'mgraddr' in vms and vm['mgraddr'] != ipAddr:		# vm, not vms
 					init = False
 					vms[i] = jsonData
 					break
