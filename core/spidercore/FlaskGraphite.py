0702b9d87446f8044583f00a724a404ed439a469
jerryj <javalove93@gmail.com>
2014-11-14 17:16:04 +0900

    Case 467:

--- core/spidercore/FlaskGraphite.py
+++ core/spidercore/FlaskGraphite.py
@@ -80,7 +80,7 @@ def mon_graphite_interface(vmid=None):
 	url = "http://localhost:8000/render/?width=500&height=500&from=-%s%s&format=json" % (timespan, timeunit)
 	if mode == 'txonly' or mode == 'both':
 		url += "&target=%s.interface.if_octets.eth*.tx" % (vmid)
-	elif mode == 'rxonly' or mode == 'both':
+	if mode == 'rxonly' or mode == 'both':
 		url += "&target=%s.interface.if_octets.eth*.rx" % (vmid)
 	result = requests.get(url).json()
 # 	for metric in result:
